87655a31bf419d395ace942adbd36686577f9fa9
Harsh Patel <hp1659414@gmail.com>
2026-02-02 14:15:54 +0530

    setup server entry point

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import App from './app';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const port = parseInt(process.env.PORT || '8080');
+
+import { ProductController } from './controllers/product.controller';
+
+// Controllers will be imported and passed here
+const controllers: any[] = [
+    new ProductController(),
+];
+
+const app = new App(controllers, port);
+
+app.listen();
