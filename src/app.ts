191c530160d4f143f51794b05fc7f476bb95a881
Harsh Patel <hp1659414@gmail.com>
2026-02-02 14:15:54 +0530

    create core application class

---
+++ src/app.ts
@@ -0,0 +1,47 @@
+import express, { Application } from 'express';
+import connectDB from './config/database';
+import { errorMiddleware } from './middlewares/error.middleware';
+
+class App {
+    public app: Application;
+    public port: number;
+
+    constructor(controllers: any[], port: number) {
+        this.app = express();
+        this.port = port;
+
+        this.connectRelationalDatabase();
+        this.initializeMiddlewares();
+        this.initializeControllers(controllers);
+    }
+
+    private connectRelationalDatabase(): void {
+        connectDB();
+    }
+
+    private initializeMiddlewares(): void {
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: true }));
+    }
+
+    private initializeControllers(controllers: any[]): void {
+        // Basic route for health check
+        this.app.get('/', (req, res) => {
+            res.send('API is running...');
+        });
+
+        controllers.forEach((controller) => {
+            this.app.use('/api', controller.router);
+        });
+
+        this.app.use(errorMiddleware);
+    }
+
+    public listen(): void {
+        this.app.listen(this.port, () => {
+            console.log(`App listening on the port ${this.port}`);
+        });
+    }
+}
+
+export default App;
