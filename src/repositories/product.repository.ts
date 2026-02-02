0a94436ab37c8665102752e792691c63589d3f17
Harsh Patel <hp1659414@gmail.com>
2026-02-02 14:15:54 +0530

    implement product repository

---
+++ src/repositories/product.repository.ts
@@ -0,0 +1,10 @@
+import { BaseRepository } from './base.repository';
+import ProductModel, { IProduct } from '../models/product.model';
+
+export class ProductRepository extends BaseRepository<IProduct> {
+    constructor() {
+        super(ProductModel);
+    }
+
+    // Add specific product queries here if needed
+}
